6f90938a6f6ab3b75e0f3774f4776504f74b15ff
James Michael <jomichael1994@gmail.com>
2024-01-24 00:03:02 +0000

    edit player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
+
 const Player = (props) => {
-	const { name, symbol } = props;
+	const { initialName, symbol } = props;
+
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const handleEditClick = () => {
+		setIsEditing((editing) => !editing);
+	};
+
+	const handleNameChange = (event) => {
+		setPlayerName(event.target.value);
+	};
+
 	return (
 		<li>
 			<span className="player">
-				<span className="player-name">{name}</span>
+				{isEditing ? (
+					<input
+						type="text"
+						value={playerName}
+						required
+						onChange={handleNameChange}
+					/>
+				) : (
+					<span className="player-name">{playerName}</span>
+				)}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleEditClick}>
+				{isEditing ? 'Save' : 'Edit'}
+			</button>
 		</li>
 	);
 };
