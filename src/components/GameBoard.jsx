2fdcaab2dce8c203f56249e54eab77ed24f85bb3
James Michael <jomichael1994@gmail.com>
2024-01-24 20:50:27 +0000

    initial game board component

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,25 @@
+const initialGameBoard = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+];
+
+const GameBoard = () => {
+	return (
+		<ol id="game-board">
+			{initialGameBoard.map((row, rowIdx) => (
+				<li key={rowIdx}>
+					<ol>
+						{row.map((playerSymbol, colIdx) => (
+							<li key={colIdx}>
+								<button>{playerSymbol}</button>
+							</li>
+						))}
+					</ol>
+				</li>
+			))}
+		</ol>
+	);
+};
+
+export default GameBoard;
